2d9258f0b153f677d24e0ee23cf9abc68620b397
Olavo Mendes <olavo.77@hotmail.com>
2020-11-15 16:54:08 -0300

    Projeto 6 adicionado

---
+++ projeto06-iluminacao_sequencial_potenciometro/projeto06-iluminacao_sequencial_potenciometro.ino
@@ -0,0 +1,37 @@
+byte pinos_led[] = {9, 10, 11, 12, 13};
+
+int delay_led;
+int direcao = 1;
+int led_atual = 0;
+int pino_potenciometro = 5;
+
+unsigned long mudanca_tempo;
+
+
+void setup() {
+    for (int x=0; x<5; x++) {
+         pinMode(pinos_led[x], OUTPUT);  
+    }
+    mudanca_tempo = millis();
+}
+
+void loop() {
+    delay_led = analogRead(pino_potenciometro);
+    
+    if ((millis() - mudanca_tempo) > delay_led) {
+        muda_led();
+        mudanca_tempo = millis();  
+    }
+}
+
+void muda_led(){
+    for (int x=0; x<5; x++) {
+         digitalWrite(pinos_led[x], LOW);  
+    }  
+
+    digitalWrite(pinos_led[led_atual], HIGH);
+    led_atual += direcao;
+
+    if (led_atual == 4) {direcao = - 1;}
+    if (led_atual == 0) {direcao = 1;}
+}
