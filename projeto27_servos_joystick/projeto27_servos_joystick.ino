955f3b93a5d04ad69e7cd4134696a441957aa7f7
Olavo Mendes <olavo.77@hotmail.com>
2020-12-06 14:03:02 -0300

    Adicionado projeto 27

---
+++ projeto27_servos_joystick/projeto27_servos_joystick.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+int pot1, pot2;
+
+
+
+void setup(){
+  servo1.attach(5);
+  servo2.attach(6);
+  
+  servo1.write(90);
+  servo2.write(90);
+}
+
+void loop(){
+  pot1 = analogRead(A3);
+  pot2 = analogRead(A4);
+  
+  pot1 = map(pot1, 0, 1023, 0, 180);
+  pot2 = map(pot2, 0, 1023, 0, 180);
+  
+  servo1.write(pot1);
+  servo2.write(pot2);
+  
+  delay(15);
+}
